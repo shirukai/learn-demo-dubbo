46a8bac0c2f3be715338bae5eab7f96dc951a0df
shirukai <308899573@qq.com>
2019-06-20 13:51:27 +0800

    1. 修改接口实现

--- dubbo-demo-api/dubbo-demo-api-provider/src/main/java/learn/demo/dubbo/api/provider/DemoServiceImpl.java
+++ dubbo-demo-api/dubbo-demo-api-provider/src/main/java/learn/demo/dubbo/api/provider/DemoServiceImpl.java
@@ -9,6 +9,6 @@ import learn.demo.dubbo.api.api.DemoService;
 public class DemoServiceImpl implements DemoService {
     @Override
     public String sayHello(String name) {
-        return "Hello " + name;
+        return "This service base in API.\nHello " + name;
     }
 }
