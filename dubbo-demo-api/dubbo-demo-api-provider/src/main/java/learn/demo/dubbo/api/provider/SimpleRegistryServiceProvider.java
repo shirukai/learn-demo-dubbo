9b3e365668d2f997f3d58a9a0c280765b1d9a153
shirukai <308899573@qq.com>
2019-06-26 17:40:43 +0800

    1. 修改注释

--- dubbo-demo-api/dubbo-demo-api-provider/src/main/java/learn/demo/dubbo/api/provider/SimpleRegistryServiceProvider.java
+++ dubbo-demo-api/dubbo-demo-api-provider/src/main/java/learn/demo/dubbo/api/provider/SimpleRegistryServiceProvider.java
@@ -5,7 +5,7 @@ import org.apache.dubbo.registry.SimpleRegistryExporter;
 /**
  * Created by shirukai on 2019-06-20 09:32
  * <p>
- * 简单注册中心服务生产者
+ * 简单注册中心服务提供者
  * 基于RegistryService接口实现的简单注册中心
  * 代码位置：
  * https://github.com/apache/dubbo/tree/master/dubbo-registry/dubbo-registry-default/src/test/java/org/apache/dubbo/registry/dubbo
